7b93cf2580ca0842cb1c4b6fc1735e9ceeda3f2a
Nico <nico.schultze97@gmail.com>
2023-12-15 13:59:21 +0900

    typedwriter animation

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function toggleDarkMode() {
   const imgSrc = isDarkMode ? 'assets/SVG/sun.svg' : 'assets/SVG/moon.svg'; // Replace with your SVG file paths
   darkModeToggle.querySelector('img').src = imgSrc;
 }
+
+// typewriter.js
+document.addEventListener("DOMContentLoaded", function() {
+  const app = document.getElementById('typewriter');
+  const typewriter = new Typewriter(app, {
+    loop: true
+  });
+
+  typewriter.typeString('Nico Schultze')
+    .pauseFor(2000) // Wait for 2 seconds at the end
+    .deleteAll() // Delete the text
+    .start();
+});
