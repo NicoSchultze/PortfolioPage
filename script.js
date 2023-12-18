912a5707fbce02d389aa645b9332060b93daf14e
Nico <nico.schultze97@gmail.com>
2023-12-18 14:06:03 +0900

    fix icon dark mode

--- script.js
+++ script.js
@@ -11,9 +11,64 @@ function toggleDarkMode() {
 
   const isDarkMode = body.classList.contains('dark-mode');
   const darkModeToggle = document.getElementById('dark-mode-toggle');
-  const imgSrc = isDarkMode ? 'assets/SVG/sun.svg' : 'assets/SVG/moon.svg'; // Replace with your SVG file paths
-  darkModeToggle.querySelector('img').src = imgSrc;
+  const iconImg = document.getElementById('icon-img');
+  const middleArrowImages = document.querySelectorAll('.middle-arrow');
+  const rightArrowImages = document.querySelectorAll('.right-arrow');
+  const githubIcon = document.querySelector('.github-icon');
+  const linkedinIcons = document.querySelectorAll('.linkedin-icon');
+  const emailIcon = document.querySelector('.email-icon');
+  const studyIcon = document.querySelector('.study-icon');
+  const experienceIcon = document.querySelector('.experience-icon');
+
+
+  let imgSrc;
+  let arrowSrc;
+  let githubSrc;
+  let linkedinSrc;
+  let emailSrc;
+  let studySrc;
+  let experienceSrc;
+
+  if (isDarkMode) {
+    imgSrc = 'assets/SVG/sun.svg'; // Dark mode icon
+    arrowSrc = 'assets/img/arrow-white.png'; // Dark mode arrow icon
+    githubSrc = 'assets/SVG/github-white.svg';
+    linkedinSrc = 'assets/SVG/linked-white.svg';
+    emailSrc = 'assets/img/email-white.png';
+    studySrc = 'assets/img/study-white.png';
+    experienceSrc = 'assets/img/experience-white.png';
+  } else {
+    imgSrc = 'assets/SVG/moon.svg'; // Light mode icon
+    arrowSrc = 'assets/img/arrow-black.png'; // light mode arrow icon
+    githubSrc = 'assets/SVG/github-black.svg';
+    linkedinSrc = 'assets/SVG/linked-black.svg';
+    emailSrc = 'assets/img/email-black.png';
+    studySrc = 'assets/img/study-black.png';
+    experienceSrc = 'assets/img/experience-black.png';
+  }
+
+  iconImg.src = imgSrc;
+  githubIcon.src = githubSrc;
+  emailIcon.src = emailSrc;
+  studyIcon.src = studySrc;
+  experienceIcon.src = experienceSrc;
+
+  linkedinIcons.forEach((linkIcon) => {
+    linkIcon.src = linkedinSrc;
+  });
+
+  // Change source for middle arrows
+  middleArrowImages.forEach((arrowImage) => {
+    arrowImage.src = arrowSrc;
+  });
+
+  // Change source for right arrows
+  rightArrowImages.forEach((arrowImage) => {
+    arrowImage.src = arrowSrc;
+  });
 }
+
+
 
 // typewriter.js
 document.addEventListener("DOMContentLoaded", function() {
